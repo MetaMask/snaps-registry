a6b530d0c6b9ee22c838e651909e8c7b0706f5f3
Florin Dzeladini <florin.dzeladini@consensys.net>
2024-09-18 10:17:56 +0200

    Add name resolution in category

--- src/index.ts
+++ src/index.ts
@@ -77,6 +77,7 @@ export const VerifiedSnapStruct = object({
         'notifications',
         'transaction insights',
         'account management',
+        'name resolution',
       ]),
     ),
     tags: optional(array(string())),
